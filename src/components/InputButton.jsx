4ff0e4951839abc1cca8452490138d56f142b286
MaAlmeida11 <matheusarameida@gmail.com>
2025-04-21 12:48:00 -0300

    criação de um componente para o card inicial e também para a input button

--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -6,6 +6,6 @@ export function InputButton({ value, setNotaAvaliacao }) {
       }
 
     return (
-        <input type="button" value={value} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(value)} />
+        <input type="button" value={value} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600 font-bold" onClick={() => handleAlterarNotaAvaliacao(value)} />
     )
 }
